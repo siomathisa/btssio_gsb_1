fe9b952f550967be7678bd36f8de2c1aa2fb548a
siomathisa <124365878+siomathisa@users.noreply.github.com>
2023-11-07 16:22:47 +0100

    Add files via upload

---
+++ gsbFrais/views/v_sommaire.php
@@ -0,0 +1,23 @@
+﻿<!-- Division pour le sommaire -->
+<nav class="menuLeft">
+    <ul class="menu-ul">
+        <li class="menu-item"><a href="index.php">retour</a></li>
+
+        <li class="menu-item">
+            Visiteur :<br>
+            <?php echo $_SESSION['prenom'] . "  " . $_SESSION['nom'] ?>
+        </li>
+
+        <li class="menu-item">
+            <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes
+                fiches de frais</a>
+        </li>
+
+        <li class="menu-item">
+            <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+        </li>
+    </ul>
+</nav>
+<section class="content">
+
+
